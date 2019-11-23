30a6616084c03a905e25de307179d11dc98e256d
libinprasanth <libinprasanth@gmail.com>
2019-11-23 19:31:50 +0530

    Updating class to className

--- components/cart.jsx
+++ components/cart.jsx
@@ -107,77 +107,77 @@ class Cart extends Component {
                   <tbody>{content}</tbody>
                 </table>
               </div>
-              <div class="row py-5 p-4 bg-white rounded shadow-sm">
-                <div class="col-lg-6">
-                  <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
+              <div className="row py-5 p-4 bg-white rounded shadow-sm">
+                <div className="col-lg-6">
+                  <div className="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
                     Coupon code
                   </div>
-                  <div class="p-4">
-                    <p class="font-italic mb-4">
+                  <div className="p-4">
+                    <p className="font-italic mb-4">
                       If you have a coupon code, please enter it in the box
                       below
                     </p>
-                    <div class="input-group mb-4 border rounded-pill p-2">
+                    <div className="input-group mb-4 border rounded-pill p-2">
                       <input
                         type="text"
                         placeholder="Apply coupon"
                         aria-describedby="button-addon3"
-                        class="form-control border-0"
+                        className="form-control border-0"
                       />
-                      <div class="input-group-append border-0">
+                      <div className="input-group-append border-0">
                         <button
                           id="button-addon3"
                           type="button"
-                          class="btn btn-dark px-4 rounded-pill"
+                          className="btn btn-dark px-4 rounded-pill"
                         >
-                          <i class="fa fa-gift mr-2" />Apply coupon
+                          <i className="fa fa-gift mr-2" />Apply coupon
                         </button>
                       </div>
                     </div>
                   </div>
-                  <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
+                  <div className="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
                     Instructions for seller
                   </div>
-                  <div class="p-4">
-                    <p class="font-italic mb-4">
+                  <div className="p-4">
+                    <p className="font-italic mb-4">
                       If you have some information for the seller you can leave
                       them in the box below
                     </p>
-                    <textarea name="" cols="30" rows="2" class="form-control" />
+                    <textarea name="" cols="30" rows="2" className="form-control" />
                   </div>
                 </div>
-                <div class="col-lg-6">
-                  <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
+                <div className="col-lg-6">
+                  <div className="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
                     Order summary{" "}
                   </div>
-                  <div class="p-4">
-                    <p class="font-italic mb-4">
+                  <div className="p-4">
+                    <p className="font-italic mb-4">
                       Shipping and additional costs are calculated based on
                       values you have entered.
                     </p>
-                    <ul class="list-unstyled mb-4">
-                      <li class="d-flex justify-content-between py-3 border-bottom">
-                        <strong class="text-muted">Order Subtotal </strong>
+                    <ul className="list-unstyled mb-4">
+                      <li className="d-flex justify-content-between py-3 border-bottom">
+                        <strong className="text-muted">Order Subtotal </strong>
                         <strong>${this.state.Subtotal}</strong>
                       </li>
-                      <li class="d-flex justify-content-between py-3 border-bottom">
-                        <strong class="text-muted">
+                      <li className="d-flex justify-content-between py-3 border-bottom">
+                        <strong className="text-muted">
                           Shipping and handling
                         </strong>
                         <strong>${this.state.shipping}</strong>
                       </li>
-                      <li class="d-flex justify-content-between py-3 border-bottom">
-                        <strong class="text-muted">Tax</strong>
+                      <li className="d-flex justify-content-between py-3 border-bottom">
+                        <strong className="text-muted">Tax</strong>
                         <strong>${this.state.tax}</strong>
                       </li>
-                      <li class="d-flex justify-content-between py-3 border-bottom">
-                        <strong class="text-muted">Total</strong>
-                        <h5 class="font-weight-bold">${this.state.total}</h5>
+                      <li className="d-flex justify-content-between py-3 border-bottom">
+                        <strong className="text-muted">Total</strong>
+                        <h5 className="font-weight-bold">${this.state.total}</h5>
                       </li>
                     </ul>
                     <a
                       href="javscript:;"
-                      class="btn btn-dark rounded-pill py-2 btn-block"
+                      className="btn btn-dark rounded-pill py-2 btn-block"
                     >
                       Procceed to checkout
                     </a>
